daed57f508bfa3459a01812dc8164a0c96aa29e8
Yahya Ali Majrashi <zyly327@gmail.com>
2026-07-26 01:35:28 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/waste")
+def waste():
+    return render_template("waste.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ templates/static/js/app.js
@@ -0,0 +1,1 @@
+console.log("CityPulse AI");
